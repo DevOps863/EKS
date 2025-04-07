e8b5779ad1c0dcd94a59e6f44020e74e1e0a9161
Panchumarthi <b.panchumarthi@accenture.com>
2025-04-07 20:38:43 +0530

    Fixed the subnet error

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "VPC" {
   vpc_name = "eks-vpc"
   igw = "my-igw"
   public_sb_count = 2
-  public_cidr = [ "10.0.1.0/24", "10.0.1.0/24" ]
+  public_cidr = [ "10.0.1.0/24", "10.0.2.0/24" ]
   public_subnet_name = "public-subnet"
   private_subnet_name = "private-subnet"
   private_cidr = [ "10.0.3.0/24", "10.0.4.0/24" ]
